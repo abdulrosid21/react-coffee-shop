67113f2481c8e3fd4d7cefdcb512e79fa707cfed
Abdul Rosid <rosid.dev@yahoo.com>
2023-03-16 00:42:30 +0700

    installation and adding some package for fresh react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="tabs">
+        <a className="tab tab-lifted">Tab 1</a>
+        <a className="tab tab-lifted tab-active">Tab 2</a>
+        <a className="tab tab-lifted">Tab 3</a>
+      </div>
+      <h1 className=" text-3xl font-bold underline italic">Hello world!</h1>
+    </>
   );
 }
-
-export default App;
