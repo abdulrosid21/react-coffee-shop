75ec406dbeb9b6c58815831cc1d6e93115023e9a
Abdul Rosid <rosid.dev@yahoo.com>
2023-03-24 11:25:39 +0700

    add some modal to input file from computer

--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -1,14 +1,64 @@
-import React from "react";
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable operator-linebreak */
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 
 function Profile() {
   const user = useSelector((state) => state.user.data.data);
+  const [setModal, setShowModal] = useState(false);
   console.log(user);
   return (
     <>
       <Navbar />
+      {setModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w- my-6 mx-auto ">
+              {/* content */}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/* header */}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">Change Image</h3>
+                  <button
+                    type="button"
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/* body */}
+                <div className="relative p-6 flex-auto">
+                  <input type="file" name="image" id="" />
+                </div>
+                {/* footer */}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black" />
+        </>
+      ) : null}
       <main className="bg-[url('/src/assets/img/coffeebreak.jpeg')] flex h-full md:h-screen w-screen bg-center bg-no-repeat bg-cover pb-10">
         <div className="h-[80%] w-[80%] justify-center m-auto flex flex-col gap-3">
           <div className="md:h-32 w-full">
@@ -21,21 +71,35 @@ function Profile() {
               <div className="w-full h-[95%] bg-primary rounded-t-xl flex flex-col p-3">
                 <div className="flex flex-col h-full justify-around">
                   <div className="flex flex-col gap-4">
-                    <input className="hidden" type="file" name="image" id="" />
-                    <img
-                      className="h-20 w-20 rounded-full object-cover object-center mx-auto"
-                      src={
-                        user.image
-                          ? process.env.REACT_APP_URL_CLOUDINARY + user.image
-                          : `${process.env.REACT_APP_URL_CLOUDINARY}Coffee%20Shop/Basic_Ui__186_rcq7oe.jpg`
-                      }
-                      alt=""
-                    />
-                    <div className="text-center">
-                      <h1 className="font-['Poppins'] font-semibold text-black text-xl">
+                    <div className="flex flex-col gap-3">
+                      <div
+                        onClick={() => setShowModal(!setModal)}
+                        className="relative w-20 h-20 rounded-full justify-center mx-auto cursor-pointer"
+                      >
+                        <div className="bg-brown h-5 w-5 flex rounded-full absolute right-0">
+                          <img
+                            className="h-2 justify-center m-auto"
+                            src={require("../../assets/img/pena.png")}
+                            alt=""
+                          />
+                        </div>
+                        <img
+                          className="h-20 w-20 rounded-full object-cover object-center mx-auto"
+                          src={
+                            user.image
+                              ? process.env.REACT_APP_URL_CLOUDINARY +
+                                user.image
+                              : `${process.env.REACT_APP_URL_CLOUDINARY}Coffee%20Shop/Basic_Ui__186_rcq7oe.jpg`
+                          }
+                          alt=""
+                        />
+                      </div>
+                      <h1 className="font-['Poppins'] font-semibold text-black text-xl justify-center mx-auto">
                         {user.display_name ? user.display_name : "New User"}
                       </h1>
-                      <h6 className="font-['Rubik'] text-xs">{user.email}</h6>
+                      <h6 className="font-['Rubik'] text-xs justify-center mx-auto">
+                        {user.email}
+                      </h6>
                     </div>
                   </div>
                   <div>
